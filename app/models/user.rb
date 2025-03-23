bb895e143cb4334a999f3e4fa538f1391d8e9597
Rafael Chiarello <rafael.cote@gmail.com>
2025-03-23 08:54:28 -0300

    add registration step

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
 
-  validates :email_address, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL }
+  validates :email_address, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
