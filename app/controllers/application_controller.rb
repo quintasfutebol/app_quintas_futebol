3173d6e4ba6201ede42bcd5342498f0b3890778f
Rafael Chiarello <rafael.cote@gmail.com>
2025-03-23 07:38:26 -0300

    add validates registration user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,8 @@
 class ApplicationController < ActionController::Base
   include Authentication
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
-
-  before_action :force_html_format
-
-  private
-
-  def force_html_format
-    request.format = :html if request.format == "*/*"
+  # Removed in development to avoid 406 errors caused by mobile simulations.
+  if Rails.env.production?
+    allow_browser versions: :modern
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
 
+  validates :email_address, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL }
+  validates :password, presence: true
+
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
